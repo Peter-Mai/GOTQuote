eefa1c55b2cd832bc64d7171f706f2ee9108e8e0
Peter Mai <peter.mai1996@gmail.com>
2025-07-22 13:43:18 -0500

    Removed choice variable and changed the url to point towards the GOT endpoint

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = 'https://api.gameofthronesquotes.xyz/v1/random'
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
